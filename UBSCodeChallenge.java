2bd1d869c6d9789fe74c6d16169f1a1881bf4707
sagarlalan <lalan.sagar@gmail.com>
2023-05-14 11:04:11 +0530

    learning
    first changes

---
+++ UBSCodeChallenge.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class UBSCodeChallenge {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world from UBSCodeChallenge ");
+        HashMap map = new HashMap<>();
+        map.put("key", "123");
+        System.out.println(map);
+    }
+    
+}
